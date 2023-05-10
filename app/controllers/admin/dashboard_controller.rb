d0cd0e7f83503b9e5d1b5bda3bc96b317e99f686
Michele Schulz <michelecaleb.schulz@gmail.com>
2023-05-10 16:45:17 +0000

    Add instance variables to show method for erb template

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,7 @@
 class Admin::DashboardController < ApplicationController
   def show
+    @productsCount = Product.count
+    @categories = Category.all
+    @categoriesCount = Category.count
   end
 end
