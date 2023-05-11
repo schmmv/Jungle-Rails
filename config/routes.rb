a078904b1a31247691dd00bf1fbbc07dc3ae6ee1
Michele Schulz <michelecaleb.schulz@gmail.com>
2023-05-11 22:24:57 +0000

    Consolidate admin namespace routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,10 @@
 Rails.application.routes.draw do
 
-  namespace :admin do
-    get 'categories/index'
-    get 'categories/new'
-    get 'categories/create'
-  end
   get 'about/index'
+  
   root to: 'products#index'
 
+ 
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
 
@@ -21,6 +18,7 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
+    resources :categories, only: [:index, :new]
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
