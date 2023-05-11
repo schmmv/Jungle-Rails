63f9b13842bf759cbf963bf5d92bec1f7b832df6
Michele Schulz <michelecaleb.schulz@gmail.com>
2023-05-11 20:35:45 +0000

    Add active sale helper

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,4 +1,7 @@
 class Sale < ApplicationRecord
+  def self.active
+    where('sales.starts_on <= ? AND sales.ends_on >= ?', Date.current, Date.current)
+  end
   def finished?
     ends_on < Date.current
   end
