50b5c9b515a4f0c28cdb5a0c364d21a6990eb7ef
Michele Schulz <michelecaleb.schulz@gmail.com>
2023-05-12 15:46:08 +0000

    Add login/logout routes for session

--- config/routes.rb
+++ config/routes.rb
@@ -24,6 +24,15 @@ Rails.application.routes.draw do
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
 
+  # log in page with form:
+	get '/login'     => 'sessions#new'
+	
+	# create (post) action for when log in form is submitted:
+	post '/login'    => 'sessions#create'
+	
+	# delete action to log out:
+	delete '/logout' => 'sessions#destroy'  
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
