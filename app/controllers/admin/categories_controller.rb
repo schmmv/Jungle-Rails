d1f2bb7217b240b3748481412da0ea4524d773c5
Michele Schulz <michelecaleb.schulz@gmail.com>
2023-05-12 00:58:41 +0000

    Fix @category query to Category instead of Product

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -8,12 +8,18 @@ class Admin::CategoriesController < ApplicationController
   end
 
   def create
-    @category = Product.new(category_name)
+    @category = Category.new(category_params)
 
     if @category.save
       redirect_to [:admin, :categories], notice: 'Category added!'
     else
       render :new
     end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
   end
 end
