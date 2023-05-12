a4342fcccca971758f40f09695115ef312136b62
Michele Schulz <michelecaleb.schulz@gmail.com>
2023-05-12 00:58:08 +0000

    Change admin categories to match admin products routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   get 'about/index'
-  
+
   root to: 'products#index'
 
  
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
-    resources :categories, only: [:index, :new]
+    resources :categories, except: [:edit, :update, :show]
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
