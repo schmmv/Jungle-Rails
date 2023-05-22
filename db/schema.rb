0db59cbb3ac3efaa31a4abea00f67c5ccd8862ac
Michele Schulz <michelecaleb.schulz@gmail.com>
2023-05-22 20:55:07 +0000

    Remove widget model migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_22_204651) do
+ActiveRecord::Schema.define(version: 2023_05_12_015947) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,11 +66,6 @@ ActiveRecord::Schema.define(version: 2023_05_22_204651) do
     t.string "name"
     t.string "email"
     t.string "password_digest"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "widgets", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
