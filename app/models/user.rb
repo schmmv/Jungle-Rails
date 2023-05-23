5af3af406314e02c8d976a96a71587281f4f613f
Michele Schulz <michelecaleb.schulz@gmail.com>
2023-05-23 22:33:10 +0000

    Add additional validations for specs to pass, add authenticate_with_credentials method

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,19 @@ class User < ApplicationRecord
   has_secure_password
 
 	# Verify that email field is not blank and that it doesn't already exist in the db (prevents duplicates):
-	validates :email, presence: true, uniqueness: true
+	validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :password_confirmation, presence: true, length: { minimum: 6 }
+  validates :name, presence: true
+
+  def self.authenticate_with_credentials(email, password)
+    email = email.downcase.strip
+    user = User.find_by_email(email)
+  
+    if user && user.authenticate(password)
+      user
+    else
+      nil
+    end
+  end
 end
