41962aa0fb2949dcbb0bd79a3591045cfe94c3c8
Michele Schulz <michelecaleb.schulz@gmail.com>
2023-05-23 19:44:10 +0000

    Add user specs outline

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'Validations' do
+    it 'is created with a password field' do
+      
+    end
+    it 'is created with a password-confirmation field' do
+
+    end
+    it 'has matching password and password-confirmation fields' do
+
+    end
+    it 'has a unique email' do
+
+    end
+    it 'has an email' do
+
+    end
+    it 'has a first name' do
+
+    end
+    it 'has a last name' do
+
+    end
+  end
+end
