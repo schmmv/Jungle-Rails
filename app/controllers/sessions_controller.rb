4127e319cfb34c91ddcc27d81d489273b50f2fe3
Michele Schulz <michelecaleb.schulz@gmail.com>
2023-05-23 22:34:32 +0000

    Adjust create method to use authenticate_with_credentials user model method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,9 +5,10 @@ class SessionsController < ApplicationController
   def create
     # Look up User in db by the email address submitted to the login form and
     # convert to lowercase to match email in db in case they had caps lock on:
-    user = User.find_by(email: params[:login][:email].downcase)
+    # user = User.find_by(email: params[:login][:email].downcase) - used prior to defining authenticate_with_credentials method in user model
     # If the user exists AND the password entered is correct.
-    if user && user.authenticate(params[:login][:password])
+    if user.authenticate_with_credentials(params[:login][:password])
+      # user && user.authenticate(params[:login][:password])
       # Save the user id inside the browser cookie. This is how we keep the user 
       # logged in when they navigate around our website.
       session[:user_id] = user.id.to_s
